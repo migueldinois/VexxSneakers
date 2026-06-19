54553e88a7f338d19a29ed6bfc28a63d4a3f2de3
migueldinois <dinoismiguel2@gmail.com>
2026-06-19 00:26:47 -0300

    refact: Simplificando rotas de produtos, corrigindo erros de execução e refatorando o catálogo de categorias, corrigindo o jinja da categoria unica

--- model/produtos.py
+++ model/produtos.py
@@ -1,10 +1,30 @@
 from database.conexao import Conexao
 
-
 class Produtos:
+    @staticmethod
     def recuperar_produtos():
         conexao, cursor = Conexao.conectar()
         cursor.execute('SELECT codigo, nome, descricao, valor, foto, banner, categoria FROM produto')
         produtos = cursor.fetchall()
+        conexao.close()
         return produtos
         
+    @staticmethod
+    def recuperar_produto_especifico(codigo):
+        conexao, cursor = Conexao.conectar()
+        cursor.execute('SELECT codigo, nome, descricao, valor, foto, banner, categoria FROM produto WHERE codigo = %s', [codigo])
+        produto = cursor.fetchone()
+        conexao.close()
+        return produto
+
+    @staticmethod
+    def recuperar_produtos_de_categoria(id_categoria):
+        conexao, cursor = Conexao.conectar()
+        cursor.execute('SELECT codigo, nome, descricao, valor, foto, banner, categoria FROM produto WHERE categoria = %s', [id_categoria])
+        produtos = cursor.fetchall()
+        conexao.close()
+        return produtos
+
+    @staticmethod
+    def recuperar_produto_por_id(id_produto):
+        return Produtos.recuperar_produto_especifico(id_produto)
