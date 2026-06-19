a5b3a911b4f2dadd441ed79d60f49bca52909484
migueldinois <dinoismiguel2@gmail.com>
2026-06-18 23:50:46 -0300

    feat: Adicionando condicao para deixar apenas usuarios logados comentar, melhorando estrutura de dados, para categorias reais, adicionando mensagem de erro nos comentarios, criação da rota de comentar e melhorando a logica de cadastro, para passar um dicionario, para nao dar conflito na hora de cadastrar o comentario

--- migrate/estrutura_dados.sql
+++ migrate/estrutura_dados.sql
@@ -1,29 +1,28 @@
 -- Criação das categorias
 INSERT INTO categorias(imagem, nome) 
-VALUES('https://picsum.photos/500/500', 'Infantil');
+VALUES('/static/src/img/banners_categorias/banner_infantil.png', 'Infantil');
 
 INSERT INTO categorias(imagem, nome) 
-VALUES('https://picsum.photos/500/500', 'Masculino');
+VALUES('/static/src/img/banners_categorias/banner_masculino.png', 'Masculino');
 
 INSERT INTO categorias(imagem, nome) 
-VALUES('https://picsum.photos/500/500', 'Feminino');
+VALUES('/static/src/img/banners_categorias/banner_feminino.png', 'Feminino');
 
 INSERT INTO categorias(imagem, nome) 
-VALUES('https://picsum.photos/500/500', 'Esportes');
-
+VALUES('/static/src/img/banners_categorias/banner_esportes.png', 'Esportes');
 
 -- Criação de itens para cada categoria
 INSERT INTO produto(nome, descricao, valor, foto, banner, categoria)
-VALUES('Produto teste 1', 'Esse produto é um produto teste da Vexx Sneakers', 290.00, 'https://picsum.photos/500/500', 'https://picsum.photos/200/300', 1)
+VALUES('Produto teste 1', 'Esse produto é um produto teste da Vexx Sneakers', 290.00, 'https://picsum.photos/500/500', 'https://picsum.photos/200/300', 1);
 
 INSERT INTO produto(nome, descricao, valor, foto, banner, categoria)
-VALUES('Produto teste 2', 'Esse produto é um produto teste da Vexx Sneakers', 220.00, 'https://picsum.photos/500/500', 'https://picsum.photos/200/300', 2)
+VALUES('Produto teste 2', 'Esse produto é um produto teste da Vexx Sneakers', 220.00, 'https://picsum.photos/500/500', 'https://picsum.photos/200/300', 2);
 
 INSERT INTO produto(nome, descricao, valor, foto, banner, categoria)
-VALUES('Produto teste 3', 'Esse produto é um produto teste da Vexx Sneakers', 120.00, 'https://picsum.photos/500/500', 'https://picsum.photos/200/300', 3)
+VALUES('Produto teste 3', 'Esse produto é um produto teste da Vexx Sneakers', 120.00, 'https://picsum.photos/500/500', 'https://picsum.photos/200/300', 3);
 
 INSERT INTO produto(nome, descricao, valor, foto, banner, categoria)
-VALUES('Produto teste 3', 'Esse produto é um produto teste da Vexx Sneakers', 430.00, 'https://picsum.photos/500/500', 'https://picsum.photos/200/300', 4)
+VALUES('Produto teste 3', 'Esse produto é um produto teste da Vexx Sneakers', 430.00, 'https://picsum.photos/500/500', 'https://picsum.photos/200/300', 4);
 
 INSERT INTO produto(nome, descricao, valor, foto, banner, categoria)
-VALUES('Produto teste 3', 'Esse produto é um produto teste da Vexx Sneakers', 430.00, 'https://picsum.photos/500/500', 'https://picsum.photos/200/300', 5)
+VALUES('Produto teste 3', 'Esse produto é um produto teste da Vexx Sneakers', 430.00, 'https://picsum.photos/500/500', 'https://picsum.photos/200/300', 4);
